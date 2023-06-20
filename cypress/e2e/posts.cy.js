6022f9b37d613191886c401fbbdaa190bf453cf3
Adai Illanes <adai.illanes@ucb.edu.bo>
2023-06-20 10:38:20 -0400

    prueba en cypress de la verificacion

--- cypress/e2e/posts.cy.js
+++ cypress/e2e/posts.cy.js
@@ -5,5 +5,11 @@ describe("POSTS", () => {
       cy.get("#amigos-button").click();
       cy.get("#amigos-div").should("contain", "realizando examen de ing de software");
     });
+    it("mostrar la pagina con la verificacion de texto vacio", () => {
+        cy.visit("/");
+        cy.get("#post").type("");
+        cy.get("#amigos-button").click();
+        cy.type(alert).should("contain","o es posible registrar un post sin un texto")
+      });
   });
   
